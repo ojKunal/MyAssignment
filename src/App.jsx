4c154abe1364c1898dee97636cf7f0501687f93b
kunal ojha <ojhakunal9631@gmail.com>
2024-05-09 02:11:25 +0530

    final-changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,15 @@ function App() {
     <Router>
      <Routes>
        <Route path='/' element = {<MyComponent/>}/>
+       <Route path='/home' element = {<MyComponent/>}/>
+       <Route path='/flashcard' element = {<MyComponent/>}/>
+       <Route path='/Contact' element = {<MyComponent/>}/>
+       <Route path='/faq' element = {<MyComponent/>}/>
+       <Route path='/study' element = {<MyComponent/>}/>
+       <Route path='/quiz' element = {<MyComponent/>}/>
+       <Route path='/test' element = {<MyComponent/>}/>
+       <Route path='/game' element = {<MyComponent/>}/>
+       <Route path='/others' element = {<MyComponent/>}/>
      </Routes>
     </Router>
  </div>
--- src/components/StudyNav/StudyNav.jsx
+++ src/components/StudyNav/StudyNav.jsx
@@ -1,25 +1,82 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const StudyNav = () => {
-    return (
-        <nav className="flex gap-5 justify-center self-center mt-14 text-xl font-medium whitespace-nowrap text-zinc-500 max-md:flex-wrap max-md:mt-10 cursor-pointer">
-          <div className="flex flex-col justify-center font-bold text-blue-950">
-            <div className="px-3 pb-3.5 border-b border-blue-950 ">Study</div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="px-3 pb-3.5">Quiz</div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="px-3 pb-3.5">Test</div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="px-3 pb-3.5">Game</div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <div className="px-3 pb-3.5">Others</div>
-          </div>
-        </nav>
-      );
-}
+  return (
+    <nav className="flex gap-5 justify-center self-center mt-14 text-xl font-medium whitespace-nowrap text-zinc-500 max-md:flex-wrap max-md:mt-10 cursor-pointer">
+      <div className="flex flex-col justify-center  ">
+        <NavLink
+          to="/study"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "cursor-pointer"
+              : isActive
+              ? "cursor-pointer  text-blue-900 border-b border-blue-950 font-bold"
+              : ""
+          }
+        >
+          <div className="px-3 pb-3.5  ">Study</div>
+        </NavLink>
+      </div>
+      <div className="flex flex-col justify-center">
+        <NavLink
+          to="/quiz"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "cursor-pointer"
+              : isActive
+              ? "cursor-pointer  text-blue-900 border-b border-blue-950 font-bold"
+              : ""
+          }
+        >
+          <div className="px-3 pb-3.5">Quiz</div>
+        </NavLink>
+      </div>
+      <div className="flex flex-col justify-center">
+        <NavLink
+          to="/test"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "cursor-pointer"
+              : isActive
+              ? "cursor-pointer  text-blue-900 border-b border-blue-950 font-bold"
+              : ""
+          }
+        >
+          <div className="px-3 pb-3.5">Test</div>
+        </NavLink>
+      </div>
+      <div className="flex flex-col justify-center">
+        <NavLink
+          to="/game"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "cursor-pointer"
+              : isActive
+              ? "cursor-pointer  text-blue-900 border-b border-blue-950 font-bold"
+              : ""
+          }
+        >
+          <div className="px-3 pb-3.5">Game</div>
+        </NavLink>
+      </div>
+      <div className="flex flex-col justify-center">
+        <NavLink
+          to="/others"
+          className={({ isActive, isPending }) =>
+            isPending
+              ? "cursor-pointer"
+              : isActive
+              ? "cursor-pointer  text-blue-900 border-b border-blue-950 font-bold"
+              : ""
+          }
+        >
+          {" "}
+          <div className="px-3 pb-3.5">Others</div>
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
-export default StudyNav
+export default StudyNav;
